dcb1792246f60e62e8babd7ae9ff1d5e5a043ecd
bestgoogoo <junta9996@gmail.com>
2023-08-28 16:30:18 +0900

    Make two flex box, just react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: "flex" }}>
+      <div style={{ backgroundColor: "teal", height: 100, width: 100 }}></div>
+      <div style={{ backgroundColor: "tomato", height: 100, width: 100 }}></div>
     </div>
   );
 }
