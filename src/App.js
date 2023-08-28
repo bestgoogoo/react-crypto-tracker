2c1f24b478091dc605e5554bc35c5b19090e38f0
bestgoogoo <junta9996@gmail.com>
2023-08-28 17:15:24 +0900

    Use more styled-components basics - Adapting props - Extending stlyes

--- src/App.js
+++ src/App.js
@@ -4,23 +4,21 @@ const Father = styled.div`
   display: flex;
 `;
 
-const BoxOne = styled.div`
-  background-color: teal;
-  width: 100;
-  height: 100;
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
+  width: 100px;
+  height: 100px;
 `;
 
-const BoxTwo = styled.div`
-  background-color: tomato;
-  width: 100;
-  height: 100;
+const Circle = styled(Box)`
+  border-radius: 50%;
 `;
 
 function App() {
   return (
     <Father>
-      <BoxOne />
-      <BoxTwo />
+      <Box bgColor="teal" />
+      <Circle bgColor="tomato" />
     </Father>
   );
 }
