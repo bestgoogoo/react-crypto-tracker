da5607e49366a57d3b36f47690dd225914c10a5a
bestgoogoo <junta9996@gmail.com>
2023-08-30 15:23:34 +0900

    Theme Typescript of ThemeProvider of styled-components - Reference typescript API of styled-components: https://styled-components.com/docs/api#typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,18 @@
-import React, { useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 function App() {
-  const [value, setValue] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setValue(value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(value);
-  };
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={value}
-          onChange={onChange}
-          type="text"
-          placeholder="username"
-        />
-        <button>Log IN</button>
-      </form>
+      <Container>
+        <H1>Theme of styled-components</H1>
+      </Container>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
+
 import App from "./App";
+import { darkMode, lightMode } from "./theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkMode}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
