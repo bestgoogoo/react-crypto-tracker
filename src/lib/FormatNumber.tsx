91b60365c388a6394d5f7d24df61312a1c871fd4
bestgoogoo <junta9996@gmail.com>
2023-08-31 17:24:12 +0900

    [Crypto Track] Coin Route - Add sreen of coin route of coin route using previous commit
    ✅ About the square brackets in the end of useEffect
    - useEffect relies on the square brackets to define the execution of a function.
    - If the square brackets are empty, the function is executed once when the component is loaded.
    - For the empty square brackets, there is a warning that < React Hook useEffect has a missing dependency: 'coinId'. >
    - To satisfy the warning, Put 'coinId' in the square brackets.
    - This means that if 'coinId' changes, the function will be executed again.
    - Since the coin is a params of the route, it does not change once the request is made
    - So the result is the same whether the square brackets are empty or the coin is included.

---
+++ src/lib/FormatNumber.tsx
@@ -0,0 +1,3 @@
+export function FormatNumber() {
+  return null;
+}
--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,6 +1,29 @@
+import styled from "styled-components";
 import { useEffect, useState } from "react";
 import { Link, useLocation, useParams } from "react-router-dom";
 import { Container, Header, ICoin, Loader, Title } from "./Coins";
+
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  span:first-child {
+    font-size: 10px;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
+`;
+const Description = styled.p`
+  margin: 20px 0px;
+`;
 
 interface RouteParams {
   coinId: string;
@@ -117,14 +140,48 @@ function Coin() {
       setPrice(priceData);
       setLoading(false);
     })();
-  }, []);
+  }, [coinId]);
   return (
     <Container>
       <Header>
-        <Title>{state?.name || "404: Not Found"}</Title>
+        <Title>
+          {state?.name ? state.name : loading ? "404: Not Found" : info?.name}
+        </Title>
       </Header>
-      <Link to={"/"}>Go Home &rarr;</Link>
-      {loading ? <Loader>Loading...</Loader> : null}
+      <Link to={"/"} style={{ display: "block", marginBottom: "10px" }}>
+        Go Home &rarr;
+      </Link>
+      {loading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Overview>
+            <OverviewItem>
+              <span>Rank:</span>
+              <span>{info?.rank}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Symbol:</span>
+              <span>$ {info?.symbol}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Open Source:</span>
+              <span>{info?.open_source ? "Yes" : "No"}</span>
+            </OverviewItem>
+          </Overview>
+          <Description>{info?.description}</Description>
+          <Overview>
+            <OverviewItem>
+              <span>Total Supply:</span>
+              <span>{price?.total_supply.toLocaleString()}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Max Supply:</span>
+              <span>{price?.max_supply.toLocaleString()}</span>
+            </OverviewItem>
+          </Overview>
+        </>
+      )}
     </Container>
   );
 }
