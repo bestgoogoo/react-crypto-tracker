80890f0ff0826fe52c451781b64b8332f81fa331
bestgoogoo <junta9996@gmail.com>
2023-09-01 02:10:32 +0900

    [Crypto Track] Nested Route(Price, Chart) - Fetch ohlcv(open high low close value) api data with useQuery, fetcher function at Chart route - Pass the data fetched from the coin route to the price route as a property

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -114,31 +114,29 @@ interface IInfoData {
   last_data_at: string;
 }
 
-interface IQuoteValue {
-  price: number;
-  volume_24h: number;
-  volume_24h_change_24h: number;
-  market_cap: number;
-  market_cap_change_24h: number;
-  percent_change_15m: number;
-  percent_change_30m: number;
-  percent_change_1h: number;
-  percent_change_6h: number;
-  percent_change_12h: number;
-  percent_change_24h: number;
-  percent_change_7d: number;
-  percent_change_30d: number;
-  percent_change_1y: number;
-  ath_price: number;
-  ath_date: string;
-  percent_from_price_ath: number;
-}
-
 interface IQuote {
-  [key: string]: IQuoteValue;
+  USD: {
+    price: number;
+    volume_24h: number;
+    volume_24h_change_24h: number;
+    market_cap: number;
+    market_cap_change_24h: number;
+    percent_change_15m: number;
+    percent_change_30m: number;
+    percent_change_1h: number;
+    percent_change_6h: number;
+    percent_change_12h: number;
+    percent_change_24h: number;
+    percent_change_7d: number;
+    percent_change_30d: number;
+    percent_change_1y: number;
+    ath_price: number;
+    ath_date: string;
+    percent_from_price_ath: number;
+  };
 }
 
-interface ITicker {
+export interface ITicker {
   id: string;
   name: string;
   symbol: string;
@@ -221,10 +219,10 @@ function Coin() {
 
           <Switch>
             <Route path={`/${coinId}/price`}>
-              <Price />
+              <Price coinId={coinId} tickersData={tickersData} />
             </Route>
             <Route path={`/${coinId}/chart`}>
-              <Chart />
+              <Chart coinId={coinId} />
             </Route>
           </Switch>
         </>
