b888cfa478ca265fe8ad9d4cc4d8c03b35975e6b
bestgoogoo <junta9996@gmail.com>
2023-09-04 22:36:28 +0900

    [To Do List] Category Change Catch by Using Recoil and JS Syntax

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,7 +1,44 @@
-import { IToDo } from "../atoms";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "../atoms";
 
-function ToDo({ text }: IToDo) {
-  return <li>{text}</li>;
+function ToDo({ id, text, category }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+    setToDos((selectToDos) => {
+      const targetIndex = selectToDos.findIndex((toDo) => toDo.id === id);
+      const selectToDo = selectToDos[targetIndex];
+      const setToDo = { id, text, category: name as any };
+      console.log(selectToDo, setToDo);
+      return [
+        ...selectToDos.slice(0, targetIndex),
+        setToDo,
+        ...selectToDos.slice(targetIndex + 1),
+      ];
+    });
+  };
+  return (
+    <li>
+      <span>{text}</span>
+      {!category.includes("TO_DO") && (
+        <button name="TO_DO" onClick={onClick}>
+          To Do
+        </button>
+      )}
+      {!category.includes("DOING") && (
+        <button name="DOING" onClick={onClick}>
+          Doing
+        </button>
+      )}
+      {!category.includes("DONE") && (
+        <button name="DONE" onClick={onClick}>
+          Done
+        </button>
+      )}
+    </li>
+  );
 }
 
 export default ToDo;
