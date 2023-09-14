883a1df7cda401345bf678e383d672404e8fc244
bestgoogoo <junta9996@gmail.com>
2023-09-14 16:39:16 +0900

    [Trello Clone] Reset to Start - npm i react-beautiful-dnd

--- src/components/Trello.tsx
+++ src/components/Trello.tsx
@@ -1,32 +1,5 @@
-import { useRecoilState } from "recoil";
-import { hourSeletor, minuteState } from "../atoms";
-
 function Trello() {
-  const [minutes, setMinutes] = useRecoilState(minuteState);
-  const [hours, setHours] = useRecoilState(hourSeletor);
-  const onChangeMinute = (event: React.FormEvent<HTMLInputElement>) => {
-    // Number(value) = +value
-    setMinutes(+event.currentTarget.value);
-  };
-  const onChangeHour = (event: React.FormEvent<HTMLInputElement>) => {
-    setHours(+event.currentTarget.value);
-  };
-  return (
-    <div>
-      <input
-        value={minutes}
-        onChange={onChangeMinute}
-        type="number"
-        placeholder="Minutes"
-      />
-      <input
-        value={hours}
-        onChange={onChangeHour}
-        type="number"
-        placeholder="Hours"
-      />
-    </div>
-  );
+  return <></>;
 }
 
 export default Trello;
