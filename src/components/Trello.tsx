3573f5aebfad0efb8a11d3d7c884652e5dffc6c5
bestgoogoo <junta9996@gmail.com>
2023-09-14 16:02:03 +0900

    [Trello Clone] Practice Recoil 1 - About get function of recoil selector - Console said Warning hours input will render a read-only field.  : You provided a `value` prop to a form field without an `onChange` handler. This will render a read-only field. If the field should be mutable use `defaultValue`. Otherwise, set either `onChange` or `readOnly`.

--- src/components/Trello.tsx
+++ src/components/Trello.tsx
@@ -1,5 +1,24 @@
+import { useRecoilState, useRecoilValue } from "recoil";
+import { hourSeletor, minuteState } from "../atoms";
+
 function Trello() {
-  return <></>;
+  const [minutes, setMinutes] = useRecoilState(minuteState);
+  const hours = useRecoilValue(hourSeletor);
+  const onChangeMinute = (event: React.FormEvent<HTMLInputElement>) => {
+    // Number(value) = +value
+    setMinutes(+event.currentTarget.value);
+  };
+  return (
+    <div>
+      <input
+        value={minutes}
+        onChange={onChangeMinute}
+        type="number"
+        placeholder="Minutes"
+      />
+      <input value={hours} type="number" placeholder="Hours" />
+    </div>
+  );
 }
 
 export default Trello;
