85b3da5198107e91f2ee9c5c6e057bbe26e88019
bestgoogoo <junta9996@gmail.com>
2023-09-14 17:10:06 +0900

    [Trello Clone] Drag and Drop Tutorial

--- src/components/Trello.tsx
+++ src/components/Trello.tsx
@@ -1,5 +1,47 @@
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+
 function Trello() {
-  return <></>;
+  const onDragEnd = () => {};
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId="one">
+        {(provided) => (
+          <ul ref={provided.innerRef} {...provided.droppableProps}>
+            <Draggable draggableId="first" index={0}>
+              {(provided) => (
+                <li ref={provided.innerRef} {...provided.draggableProps}>
+                  <span {...provided.dragHandleProps}>👋</span>
+                  One
+                </li>
+              )}
+            </Draggable>
+            <Draggable draggableId="second" index={1}>
+              {(provided) => (
+                <li
+                  ref={provided.innerRef}
+                  {...provided.draggableProps}
+                  {...provided.dragHandleProps}
+                >
+                  Two
+                </li>
+              )}
+            </Draggable>
+            <Draggable draggableId="third" index={2}>
+              {(provided) => (
+                <li
+                  ref={provided.innerRef}
+                  {...provided.draggableProps}
+                  {...provided.dragHandleProps}
+                >
+                  Three
+                </li>
+              )}
+            </Draggable>
+          </ul>
+        )}
+      </Droppable>
+    </DragDropContext>
+  );
 }
 
 export default Trello;
